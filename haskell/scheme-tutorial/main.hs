b82837f8e9099267a9b49d9ebbc44b5aa1667274
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2020-02-18 12:12:31 +0100

    [FEATURE] Started writing scheme in 48 hours tutorial

---
+++ haskell/scheme-tutorial/main.hs
@@ -0,0 +1,11 @@
+-- main.hs
+-- Haskell tutorial from here:
+-- https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours/First_Steps
+
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn ("Hello, " ++ args !! 0)
