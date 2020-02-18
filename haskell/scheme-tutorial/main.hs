de55e7984252a6f4c523fb951a247cee2bc0b818
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2020-02-18 19:21:26 +0100

    [FEATURE] Finished parsing chapter. Started evaluation

--- haskell/scheme-tutorial/main.hs
+++ haskell/scheme-tutorial/main.hs
@@ -6,6 +6,7 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
+import Numeric
 
 data LispVal = Atom String
              | List [LispVal]
@@ -13,7 +14,9 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
-             deriving (Show)
+             | Character Char
+
+instance Show LispVal where show = showVal
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -23,6 +26,13 @@ spaces = skipMany1 space
 
 parseSpecialChar :: Parser Char
 parseSpecialChar = char '\\' >> oneOf "ntr\\\""
+
+parseChar :: Parser LispVal
+parseChar = do
+  char '#'
+  char '\\'
+  c <- letter <|> symbol <|> digit
+  return $ Character c
 
 parseString :: Parser LispVal
 parseString = do
@@ -43,26 +53,77 @@ parseAtom = do
     "#f" -> Bool False
     _    -> Atom atom
 
+parseDecimal :: Parser LispVal
+parseDecimal = Number <$> (read <$> many1 digit)
+
+bintodec :: Integral i => i -> i
+bintodec 0 = 0
+bintodec i = 2 * bintodec (div i 10) + (mod i 10)
+
+parseWithBase :: Parser LispVal
+parseWithBase = do
+  char '#'
+  base <- oneOf "boxd"
+  retval <- case base of
+    'b' -> bintodec . read <$> many1 digit
+    'o' -> fst . head . readOct <$> many1 digit
+    'x' -> fst . head . readHex <$> many (oneOf "0123456789abcdefABCDEF") 
+    'd' -> read <$> many1 digit
+  return $ Number retval
   
 
 parseNumber :: Parser LispVal
+parseNumber = parseWithBase <|> parseDecimal
 -- parseNumber = Number <$> (read <$> many1 digit)
 -- parseNumber = read <$> many1 digit >>= return . Number
-parseNumber = do
-  val <- read <$> many1 digit
-  return $ Number val
+-- parseNumber = do
+--   val <- read <$> many1 digit
+--   return $ Number val
 
 -- parseNumber = liftM (Number . read) $ many1 digit
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
-parseExpr =  parseAtom
-         <|> parseNumber
+parseExpr =  parseNumber
+         <|> parseChar
+         <|> parseAtom
          <|> parseString
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
   Left  err -> "No Match: " ++ show err
-  Right val -> "Found value" ++ show val
+  Right val -> "Found value: " ++ show val
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 main :: IO ()
 main = do
