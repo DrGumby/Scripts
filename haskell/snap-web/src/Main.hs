e9c5c4c0a51d974f68ea6cca160933b4c6a2e7dd
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2020-03-12 23:13:30 +0100

    [FEATURE] Haskell web server

---
+++ haskell/snap-web/src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Applicative ((<|>))
+import Snap.Core
+import Snap.Http.Server (httpServe, setPort)
+import Snap.Util.FileServe (serveFile)
+import System.Environment (lookupEnv)
+
+showInfo :: MonadSnap m => m ()
+showInfo = sendFile "views/index.html"
+
+main :: IO ()
+main = do
+    mbPort <- lookupEnv "PORT"
+    let port = maybe 8000 read mbPort
+        config = setPort port mempty
+    httpServe config $ 
+        ifTop (serveFile "views/index.html")
+        <|> route
+            [ ("/info", method GET showInfo)
+            ]
